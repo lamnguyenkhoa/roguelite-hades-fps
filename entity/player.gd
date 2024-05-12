d8a7435fdaff6835842cae864c0aff1c69b22abb
NKLam <khoalamvn@gmail.com>
2024-05-12 20:33:18 +1000

    Slight improve air control

--- entity/player.gd
+++ entity/player.gd
@@ -9,7 +9,8 @@ class_name Player
 var landing = preload ("res://asset/sfx/jump_landing.wav")
 
 const MAX_SPEED = 10.0
-const ACCEL = 50.0
+const MAX_FALL_SPEED = 100.0
+const ACCEL_RATE = 50.0
 const JUMP_FORCE = 7
 const RAY_REACH = 0.1
 const MOUSE_SENS = 0.005
@@ -19,6 +20,8 @@ var floor_col_pos = Vector3.ZERO
 var jumped = false
 var prev_pos = Vector3.ZERO
 var camera_height = 0
+var vel_horizontal = Vector2(0, 0)
+var vel_vertical = 0
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -28,6 +31,9 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotate_player(event)
+	if event.is_action_pressed("jump") and grounded():
+		vel_vertical = JUMP_FORCE
+		jumped = true
 
 func _process(delta):
 	interpolate_camera_pos(delta)
@@ -36,37 +42,31 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	input_dir = input_dir.rotated( - rotation.y)
 
-	var vel_horizontal = Vector2(velocity.x, velocity.z)
-	var vel_vertical = velocity.y
-	
 	# If the next line is for grounded only, we will have bunnyhop tech
 	# If not move, gradually reduce movespeed to 0
-	vel_horizontal -= vel_horizontal.normalized() * (ACCEL / 2) * delta
+	vel_horizontal -= vel_horizontal.normalized() * (ACCEL_RATE / 2) * delta
+	# Stand still
+	if vel_horizontal.length_squared() < 1.0 and input_dir.length_squared() < 0.01:
+		vel_horizontal = Vector2.ZERO
 
 	if grounded():
 		if jumped:
 			audio_player.stream = landing
 			audio_player.play()
 			jumped = false
-		# Stand still
-		if vel_horizontal.length_squared() < 1.0 and input_dir.length_squared() < 0.01:
-			vel_horizontal = Vector2.ZERO
 	else:
 		vel_vertical -= GRAVITY * delta
+		vel_vertical = clamp(vel_vertical, -MAX_FALL_SPEED, 10000)
 
 	# Use the next line will make player move faster when strafing + rotate camera
 	# var current_speed = vel_horizontal.dot(input_dir)
 	var current_speed = vel_horizontal.length()
-	var add_speed = clamp(MAX_SPEED - current_speed, 0.0, ACCEL * delta)
+	var add_speed = clamp(MAX_SPEED - current_speed, 0.0, ACCEL_RATE * delta)
 	vel_horizontal += input_dir * add_speed
-		
-	if (Input.is_action_pressed("jump") or Input.is_action_just_pressed("jump")) and grounded():
-		vel_vertical = JUMP_FORCE
-		jumped = true
 
 	velocity = Vector3(vel_horizontal.x, vel_vertical, vel_horizontal.y)
 	var speedometer = snapped(vel_horizontal.length(), 0.1)
-	speed_label.text = "{0} u/s".format([speedometer])
+	speed_label.text = "HSpeed: {0} u/s\nVSpeed: {1} u/s".format([speedometer, snapped(vel_vertical, 0.1)])
 	move_and_slide()
 
 func grounded():
