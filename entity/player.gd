b003eb64443aa992df051668261270e2048680e8
NKLam <khoalamvn@gmail.com>
2024-05-19 22:19:11 +1000

    Improve sliding and debug label

--- entity/player.gd
+++ entity/player.gd
@@ -34,8 +34,6 @@ const SLIDE_SPEED = 5
 
 var floor_col_pos = Vector3.ZERO
 var jumped = false
-var prev_pos = Vector3.ZERO
-var camera_height = 0
 var vel_horizontal = Vector2(0, 0)
 var vel_vertical = 0
 var is_dashing = false
@@ -51,8 +49,6 @@ var slide_dir = Vector2(0, 0)
 
 func _ready():
     Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-    prev_pos = player_camera.position
-    camera_height = player_camera.position.y
     gun_container_offset = gun_container.position
     last_dashed_timestamp = Time.get_ticks_msec() - dash_cd * 1000
 
@@ -70,9 +66,6 @@ func _input(event):
     if event.is_action_pressed("secondary_attack"):
         secondary_attack()
 
-func _process(delta):
-    interpolate_camera_pos(delta)
-
 func _physics_process(delta):
     if is_dashing or is_sliding:
         if raw_input_dir == Vector2.ZERO:
@@ -83,7 +76,7 @@ func _physics_process(delta):
         input_dir = raw_input_dir.rotated( - rotation.y)
 
     # If the next line is for grounded only, we will have bunnyhop tech
-    # If not move, gradually reduce movespeed to 0
+    # If not move, gradually reduce movespeed to 0 (speed decay)
     vel_horizontal -= vel_horizontal.normalized() * (ACCEL_RATE / 2) * delta
     # Stand still
     if vel_horizontal.length_squared() < 1.0 and input_dir.length_squared() < 0.01:
@@ -121,24 +114,31 @@ func _physics_process(delta):
         bonus_speed = SLIDE_SPEED
     else:
         bonus_speed = lerpf(bonus_speed, 0, delta * 10)
-    velocity += velocity.normalized() * bonus_speed
-    var h_speed = snapped(vel_horizontal.length() + bonus_speed, 0.1)
-    var v_speed = snapped(vel_vertical, 0.1)
-    debug_label.text = "HSpeed: {0} u/s\nVSpeed: {1} u/s".format([h_speed, v_speed])
-    debug_label.text += "\nOn ground: {0}\nOn wall: {1}".format([is_on_floor(), is_on_wall_only()])
-    debug_label.text += "\nIs dashing: {0}\nIs sliding: {1}".format([is_dashing, is_sliding])
-    debug_label.text += "\nAir jumps left: {0}".format([max_air_jump - current_air_jump_count])
+
+    var velocity_dir = velocity.normalized()
+    velocity += Vector3(velocity_dir.x, 0, velocity_dir.z) * bonus_speed
+
     move_and_slide()
 
+    show_debug_label()
     var gun_sway_velocity = velocity * transform.basis
     gun_container.position = lerp(gun_container.position, gun_container_offset - (gun_sway_velocity / 500), delta * 10)
-
     camera_tilt(delta)
+
+func show_debug_label():
+    var h_speed = snapped(Vector3(velocity.x, 0, velocity.z).length(), 0.1)
+    var v_speed = snapped(vel_vertical, 0.1)
+    debug_label.text = "HSpeed: {0} u/s\nVSpeed: {1} u/s".format([h_speed, v_speed])
+    debug_label.text += "\nOn ground: {0} | On wall: {1}".format([is_on_floor(), is_on_wall_only()])
+    debug_label.text += "\nIs dashing: {0} | Is sliding: {1}".format([is_dashing, is_sliding])
+    debug_label.text += "\nAir jumps left: {0}".format([max_air_jump - current_air_jump_count])
 
 func jump():
     vel_vertical = JUMP_FORCE
     jumped = true
     state_chart.send_event("jump")
+    is_dashing = false
+    is_sliding = false
 
 func primary_attack():
     var gun: Gun = gun_container.get_child(0)
@@ -166,16 +166,6 @@ func perform_attack(gun: Gun):
         bullet_inst.init(gun.barrel.global_position, aim_ray_end.global_position)
     get_parent().add_child(bullet_inst)
 
-func ground_slide():
-    return
-
-
-func interpolate_camera_pos(delta):
-    var camera_pos = prev_pos.lerp(position, delta * 70)
-    player_camera.global_position = camera_pos
-    player_camera.position.y = camera_height
-    prev_pos = camera_pos
-
 func rotate_player(event):
     rotate(Vector3(0, -1, 0), event.relative.x * (GameManager.mouse_sensitivity / 10000))
     player_camera.rotate_x( - event.relative.y * (GameManager.mouse_sensitivity / 10000))
@@ -191,24 +181,29 @@ func camera_tilt(delta):
     else:
         neck.rotation.z = lerp(neck.rotation.z, deg_to_rad(0), delta * 5)
 
+        neck.position.y = lerp(neck.position.y, -1.0, delta * 5)
+    if is_sliding:
+        neck.position.y = lerp(neck.position.y, -1.0, delta * 5)
+    else:
+        neck.position.y = lerp(neck.position.y, 0.0, delta * 5)
+
 func _on_dash_duration_timeout() -> void:
     is_dashing = false
 
-func _on_grounded_state_input(event:InputEvent):
+func _on_grounded_state_input(event: InputEvent):
     if event.is_action_pressed("jump"):
         jump()
 
-func _on_airborne_state_input(event:InputEvent):
+func _on_airborne_state_input(event: InputEvent):
     if event.is_action_pressed("jump") and current_air_jump_count < max_air_jump:
         current_air_jump_count += 1
         jump()
 
-
-func _on_grounded_state_physics_processing(delta:float):
+func _on_grounded_state_physics_processing(_delta: float):
     if Input.is_action_pressed("crouch"):
         is_sliding = true
-        neck.position.y = lerp(neck.position.y, -1.0, delta * 5)
     else:
         is_sliding = false
-        neck.position.y = lerp(neck.position.y, 0.0, delta * 5)
 
+func _on_airborne_state_entered() -> void:
+    is_sliding = false
