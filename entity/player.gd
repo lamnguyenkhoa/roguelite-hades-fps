b945f348ac6ea817b93f4467c8b870b0bdc4ba80
NKLam <khoalamvn@gmail.com>
2024-05-19 15:04:13 +1000

    WIP from PY

--- entity/player.gd
+++ entity/player.gd
@@ -30,6 +30,7 @@ const MOUSE_SENS = 0.005
 const GRAVITY = 14
 
 const DASH_SPEED = 20
+const SLIDE_SPEED = 7
 
 var floor_col_pos = Vector3.ZERO
 var jumped = false
@@ -38,6 +39,7 @@ var camera_height = 0
 var vel_horizontal = Vector2(0, 0)
 var vel_vertical = 0
 var is_dashing = false
+var is_sliding = false
 var bonus_speed = 0
 var raw_input_dir = Vector2(0, 0)
 var input_dir = Vector2(0, 0)
@@ -45,6 +47,7 @@ var input_dir = Vector2(0, 0)
 var gun_container_offset: Vector3
 var last_dashed_timestamp
 var current_air_jump_count = 0
+var slide_dir = Vector2(0, 0)
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -56,8 +59,6 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotate_player(event)
-	if event.is_action_pressed("jump") and is_on_floor():
-		jump()
 	if event.is_action_pressed("dash"):
 		if last_dashed_timestamp + dash_cd * 1000 <= Time.get_ticks_msec():
 			last_dashed_timestamp = Time.get_ticks_msec()
@@ -73,7 +74,11 @@ func _process(delta):
 	interpolate_camera_pos(delta)
 
 func _physics_process(delta):
-	if not is_dashing:
+	if is_dashing or is_sliding:
+		if raw_input_dir == Vector2.ZERO:
+			raw_input_dir = Vector2(0, -1)
+			input_dir = raw_input_dir.rotated( - rotation.y)
+	if not is_dashing and not is_sliding:
 		raw_input_dir = Input.get_vector("left", "right", "up", "down")
 		input_dir = raw_input_dir.rotated( - rotation.y)
 
@@ -102,12 +107,18 @@ func _physics_process(delta):
 	# var current_speed = vel_horizontal.dot(input_dir)
 	var current_speed = vel_horizontal.length()
 	var add_speed = clamp(MAX_SPEED - current_speed, 0.0, ACCEL_RATE * delta)
-	vel_horizontal += input_dir * add_speed
+
+	if is_dashing or is_sliding:
+		vel_horizontal = input_dir * MAX_SPEED
+	else:
+		vel_horizontal += input_dir * add_speed
 
 	velocity = Vector3(vel_horizontal.x, vel_vertical, vel_horizontal.y)
 
 	if is_dashing:
 		bonus_speed = DASH_SPEED
+	elif is_sliding:
+		bonus_speed = SLIDE_SPEED
 	else:
 		bonus_speed = lerpf(bonus_speed, 0, delta * 10)
 	velocity += velocity.normalized() * bonus_speed
@@ -154,6 +165,10 @@ func perform_attack(gun: Gun):
 		bullet_inst.init(gun.barrel.global_position, aim_ray_end.global_position)
 	get_parent().add_child(bullet_inst)
 
+func ground_slide():
+	return
+
+
 func interpolate_camera_pos(delta):
 	var camera_pos = prev_pos.lerp(position, delta * 70)
 	player_camera.global_position = camera_pos
@@ -178,8 +193,16 @@ func camera_tilt(delta):
 func _on_dash_duration_timeout() -> void:
 	is_dashing = false
 
+func _on_grounded_state_input(event:InputEvent):
+	if event.is_action_pressed("jump"):
+		jump()
 
 func _on_airborne_state_input(event:InputEvent):
 	if event.is_action_pressed("jump") and current_air_jump_count < max_air_jump:
 		current_air_jump_count += 1
 		jump()
+
+
+func _on_grounded_state_physics_processing(delta:float):
+	if Input.is_action_pressed("crouch"):
+		ground_slide()
