2dbc23d3d4f649af0be9ce5cfd5d79274967c7a9
NKLam <khoalamvn@gmail.com>
2024-06-14 22:08:50 +1000

    Add window, resolution and 3d scaling options

--- singleton/enum_autoload.gd
+++ singleton/enum_autoload.gd
@@ -1,11 +1,12 @@
 extends Node
 
 enum GunSecondaryAttackType {
-    NONE,
-    CLICK_ATTACK, # ex: Shoot a strong beam once
-    CLICK_NONATTACK, # ex: Buff primary
-    HOLD, # ex: Use a shield / shooting lots of bullets
-    HOLD_AND_RELEASE # ex: Charge a powerful beam and shoot
+	NONE,
+	CLICK_ATTACK, # ex: Shoot a strong beam once
+	CLICK_NONATTACK, # ex: Buff primary
+	HOLD, # ex: Use a shield / shooting lots of bullets
+	HOLD_AND_RELEASE # ex: Charge a powerful beam and shoot
 }
 
 var FPS_LIMIT_ARRAY = [30, 60, 120, 144, 240, 0]
+var RESOLUTION_ARRAY = [Vector2i(2560, 1440), Vector2i(1920, 1080), Vector2i(1440, 900), Vector2i(1366, 768), Vector2i(1280, 720), Vector2i(1024, 768)]
--- ui/setting_ui.gd
+++ ui/setting_ui.gd
@@ -10,6 +10,10 @@ class_name SettingUI
 @onready var camera_tilt_toggle: CheckButton = $TabContainer/Graphic/VBoxContainer/CameraTilt/CameraTiltToggle
 @onready var fps_limit_option_button: OptionButton = $TabContainer/Graphic/VBoxContainer/FPSLimit/FPSLimitOptionButton
 @onready var vsync_option_button: OptionButton = $TabContainer/Graphic/VBoxContainer/Vsync/VsyncOptionButton
+@onready var window_mode_option_button: OptionButton = $TabContainer/Graphic/VBoxContainer/WindowMode/WindowModeOptionButton
+@onready var resolution_option_button: OptionButton = $TabContainer/Graphic/VBoxContainer/Resolution/ResolutionOptionButton
+@onready var scaling_3d_slider: HSlider = $TabContainer/Graphic/VBoxContainer/Scaling3D/Scaling3DSlider
+@onready var scaling_3d_value: Label = $TabContainer/Graphic/VBoxContainer/Scaling3D/Value
 
 @onready var master_slider: HSlider = $TabContainer/Audio/VBoxContainer/Master/MasterSlider
 @onready var master_value: Label = $TabContainer/Audio/VBoxContainer/Master/Value
@@ -25,15 +29,31 @@ var pause_ui: PauseUI
 func _ready() -> void:
 	visible = false
 	pause_ui = get_parent()
+
 	mouse_sen_slider.value = GameManager.mouse_sensitivity
 	mouse_sen_value.text = "{0}".format([GameManager.mouse_sensitivity])
+
 	fov_slider.value = GameManager.camera_fov
 	fov_value.text = "{0}".format([GameManager.camera_fov])
+
 	camera_tilt_toggle.set_pressed_no_signal(GameManager.camera_tilt)
+
 	Engine.max_fps = EnumAutoload.FPS_LIMIT_ARRAY[GameManager.fps_limit_index]
 	fps_limit_option_button.selected = GameManager.fps_limit_index
+
 	DisplayServer.window_set_vsync_mode(GameManager.vsync_option_index)
 	vsync_option_button.selected = GameManager.vsync_option_index
+
+	DisplayServer.window_set_size(EnumAutoload.RESOLUTION_ARRAY[GameManager.resolution_index])
+	resolution_option_button.selected = GameManager.resolution_index
+
+	set_window_mode(GameManager.window_mode_index)
+	window_mode_option_button.selected = GameManager.window_mode_index
+
+	get_viewport().set_scaling_3d_scale(GameManager.scaling_3d / 100.0)
+	scaling_3d_slider.value = GameManager.scaling_3d
+	scaling_3d_value.text = "{0}%".format([GameManager.scaling_3d])
+
 	SoundManager.set_master_volume(GameManager.master_audio / 100.0)
 	SoundManager.set_music_volume(GameManager.bgm_audio / 100.0)
 	SoundManager.set_sound_volume(GameManager.sfx_audio / 100.0)
@@ -60,7 +80,7 @@ func _on_control_option_pressed() -> void:
 func _on_graphic_option_pressed() -> void:
 	tab_container.current_tab = 1
 	SoundManager.play_button_click_sfx()
-	
+
 func _on_audio_option_pressed() -> void:
 	tab_container.current_tab = 2
 	SoundManager.play_button_click_sfx()
@@ -92,6 +112,12 @@ func _on_vsync_option_button_item_selected(index: int) -> void:
 	DisplayServer.window_set_vsync_mode(index)
 	GameManager.vsync_option_index = index
 
+func _on_resolution_option_button_item_selected(index: int) -> void:
+	SoundManager.play_button_click_sfx()
+	DisplayServer.window_set_size(EnumAutoload.RESOLUTION_ARRAY[index])
+	GameManager.resolution_index = index
+	centre_window()
+
 func _on_master_slider_value_changed(value: float) -> void:
 	SoundManager.set_master_volume(value / 100.0)
 	master_value.text = "{0}".format([value])
@@ -112,47 +138,42 @@ func _on_bgm_slider_value_changed(value: float) -> void:
 	bgm_value.text = "{0}".format([value])
 	GameManager.bgm_audio = value
 
-func _on_audio_option_mouse_entered() -> void:
+func play_button_hover_sfx():
 	SoundManager.play_button_hover_sfx()
 
-func _on_graphic_option_mouse_entered() -> void:
-	SoundManager.play_button_hover_sfx()
+func set_window_mode(index: int) -> void:
+	match index:
+		0: # Fullscreen
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+			resolution_option_button.disabled = true
+			var resolution_text = str(get_window().get_size().x) + "x" + str(get_window().get_size().y)
+			resolution_option_button.set_text(resolution_text)
+		1: # Windowed
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+			DisplayServer.window_set_size(EnumAutoload.RESOLUTION_ARRAY[GameManager.resolution_index])
+			centre_window()
+			resolution_option_button.disabled = false
+			resolution_option_button.selected = GameManager.resolution_index
+		2: # Borderless windowed
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+			DisplayServer.window_set_size(EnumAutoload.RESOLUTION_ARRAY[GameManager.resolution_index])
+			centre_window()
+			resolution_option_button.disabled = false
+			resolution_option_button.selected = GameManager.resolution_index
 
-func _on_control_option_mouse_entered() -> void:
-	SoundManager.play_button_hover_sfx()
+func centre_window():
+	var centre_screen = DisplayServer.screen_get_position() + DisplayServer.screen_get_size() / 2
+	var window_size = get_window().get_size_with_decorations()
+	get_window().set_position(centre_screen - window_size / 2)
 
-func _on_back_button_mouse_entered() -> void:
-	SoundManager.play_button_hover_sfx()
+func _on_window_mode_option_button_item_selected(index: int) -> void:
+	set_window_mode(index)
+	GameManager.window_mode_index = index
 
-func _on_ui_slider_mouse_entered() -> void:
-	SoundManager.play_button_hover_sfx()
-
-func _on_sfx_slider_mouse_entered() -> void:
-	SoundManager.play_button_hover_sfx()
-
-func _on_bgm_slider_mouse_entered() -> void:
-	SoundManager.play_button_hover_sfx()
-
-func _on_master_slider_mouse_entered() -> void:
-	SoundManager.play_button_hover_sfx()
-
-func _on_vsync_option_button_mouse_entered() -> void:
-	SoundManager.play_button_hover_sfx()
-
-func _on_vsync_option_button_item_focused(_index: int) -> void:
-	SoundManager.play_button_hover_sfx()
-
-func _on_fps_limit_option_button_item_focused(_index: int) -> void:
-	SoundManager.play_button_hover_sfx()
-
-func _on_fps_limit_option_button_mouse_entered() -> void:
-	SoundManager.play_button_hover_sfx()
-
-func _on_camera_tilt_toggle_mouse_entered() -> void:
-	SoundManager.play_button_hover_sfx()
-
-func _on_fov_slider_mouse_entered() -> void:
-	SoundManager.play_button_hover_sfx()
-
-func _on_mouse_sen_slider_mouse_entered() -> void:
-	SoundManager.play_button_hover_sfx()
+func _on_scaling_3d_slider_value_changed(value: float) -> void:
+	GameManager.scaling_3d = value
+	get_viewport().set_scaling_3d_scale(value / 100.0)
+	scaling_3d_value.text = "{0}%".format([value])
