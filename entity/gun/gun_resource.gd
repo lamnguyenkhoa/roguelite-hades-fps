352be68ce02e7c7fd0fd91d9b51bc35bc7390c38
NKLam <khoalamvn@gmail.com>
2024-06-23 14:27:14 +1000

    Add dummy enemy

--- entity/gun/gun_resource.gd
+++ entity/gun/gun_resource.gd
@@ -4,8 +4,7 @@ class_name GunResource
 @export var name: String
 @export_multiline var description: String
 
-# Stat
-@export var damage: float
+@export_group("Stat")
 ## Bullet per second
 @export var firerate: float
 ## How fast bullet travel. Ignored if is_hitscan ticked
@@ -14,12 +13,14 @@ class_name GunResource
 @export var is_hitscan: bool
 
 @export_group("Primary")
+@export var primary_damage: int
 @export var primary_sfx: AudioStream
 @export var primary_bounce_time = 0
 @export var primary_pierce = false
 @export var primary_screenshake: float = 0
 
 @export_group("Secondary")
+@export var secondary_damage: int
 @export var secondary_type: EnumAutoload.GunSecondaryAttackType
 @export var secondary_sfx: AudioStream
 @export var secondary_cooldown: float = 0.5
