69c37bb9ba849ca9b3813c6b0f2a65fa4850b8f7
NKLam <khoalamvn@gmail.com>
2024-06-30 12:43:03 +1000

    Add texture and detail to 1st level

--- entity/gun/projectiles/gun_hitscan.gd
+++ entity/gun/projectiles/gun_hitscan.gd
@@ -7,8 +7,7 @@ class_name GunHitscan
 @onready var shot_flash_start = $ShotFlashStart
 
 var alpha = 1.0
-
-const FADE_SPEED = 4
+var fade_speed = 4.0
 
 func _ready():
 	var dup_mat = material_override.duplicate()
@@ -18,12 +17,13 @@ func _ready():
 		shot_flash_start.rotate_z(rotate_amount)
 		shot_flash_start.modulate = material_override.albedo_color
 
-func init(pos1: Vector3, pos2: Vector3):
+func init(pos1: Vector3, pos2: Vector3, _fade_speed: float = 4.0):
+	fade_speed = _fade_speed
 	self.scale = Vector3(0.01 * thickness, 0.01 * thickness, pos1.distance_to(pos2))
 	self.look_at_from_position((pos1 + pos2) / 2, pos2, Vector3.UP)
 
 func _process(delta):
-	alpha -= delta * FADE_SPEED
+	alpha -= delta * fade_speed
 	alpha = clamp(alpha, 0, 1)
 	material_override.albedo_color.a = alpha
 	if shot_flash_start:
